96dd60f41ade90addc5ada4dc60b23d87bcf4eca
shujon11223366 <52807970+shujon11223366@users.noreply.github.com>
2025-07-21 17:40:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -35,3 +35,9 @@ def generate():
 
     # Return file
     return send_file(audio_file, as_attachment=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
